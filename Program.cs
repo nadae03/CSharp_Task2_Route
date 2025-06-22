1b958046e281cd0b44c800af44de8f6570b8419b
nada emad <nada emad@DESKTOP-EIGB059>
2025-06-23 00:14:33 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SessionsTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
